40fbc7d28303ad04beb3f47b6efe1e2989b07a91
foy4748 <faisaljfcl@gmail.com>
2023-05-18 17:23:00 +0600

    Auto Create and Delete Operations are implemented

--- mysite/autos/urls.py
+++ mysite/autos/urls.py
@@ -5,5 +5,9 @@ from . import views
 app_name = "autos"
 urlpatterns = [
         path("", views.AutoView.as_view(), name="autos"),
-        path("/create", views.AutoCreateView.as_view(), name="autos-create"),
+        path("auto-create/", views.AutoCreateView.as_view(), name="auto-create"),
+        path("make-create/", views.MakeCreateView.as_view(), name="make-create"),
+
+        path("<int:pk>/auto-delete", views.AutoDeleteView.as_view(), name="auto-delete"),
+        path("<int:pk>/make-delete", views.MakeDeleteView.as_view(), name="make-delete"),
 ]
--- mysite/autos/views.py
+++ mysite/autos/views.py
@@ -1,13 +1,51 @@
 from django.shortcuts import render
 from django.views.generic import ListView, CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
+from django.contrib.auth.mixins import LoginRequiredMixin
+
 from .models import Auto, Make
 
 # Create your views here.
-class AutoView(ListView):
-    model = Auto
-    template_name = "autos/auto_list.html"
 
-class AutoCreateView(CreateView):
+# READ
+class AutoView(ListView):
+    template_name = "autos/auto_list.html"
+    def get(self, request):
+        auto_list = Auto.objects.all()
+        make_list = Make.objects.all()
+        ctx = { "auto_list": auto_list, "make_list": make_list}
+        return render(request, self.template_name, ctx)
+
+# CREATE
+class AutoCreateView(LoginRequiredMixin, CreateView):
     model = Auto
-    template_name = "autos/auto_form.html"
+    template_name = "autos/auto_form.html" 
     fields = "__all__"
+
+    def get_success_url(self):
+        return reverse_lazy('autos:autos')
+
+class MakeCreateView(LoginRequiredMixin, CreateView):
+    model = Make
+    template_name = "autos/make_form.html"
+    fields = "__all__"
+    
+    def get_success_url(self):
+        return reverse_lazy('autos:autos')
+
+# DELETE
+class AutoDeleteView(LoginRequiredMixin, DeleteView):
+    model = Auto
+    template_name = "autos/auto_confirm_delete.html"
+    fields = "__all__"
+
+    def get_success_url(self):
+        return reverse_lazy('autos:autos')
+
+class MakeDeleteView(LoginRequiredMixin, DeleteView):
+    model = Make
+    template_name = "autos/make_confirm_delete.html"
+    fields = "__all__"
+
+    def get_success_url(self):
+        return reverse_lazy('autos:autos')

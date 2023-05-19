6a0860aac9e103ff2824d30020a7cb0d25f80bbe
foy4748 <faisaljfcl@gmail.com>
2023-05-19 17:41:07 +0600

    Cats CRUD | Update and Delete Views and Urls for Breed

--- mysite/cats/views.py
+++ mysite/cats/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import ListView, View
 from django.views.generic.edit  import CreateView, UpdateView, DeleteView
 from django.urls import reverse_lazy
 from django.contrib.auth.mixins import LoginRequiredMixin
@@ -9,12 +9,47 @@ from .models import Cat, Breed
 # Create your views here.
 
 # READ
-class CatList(LoginRequiredMixin, ListView):
-    model = Cat
+class CatList(LoginRequiredMixin, View):
     template_name = "cats/cat_list.html"
+    def get(self, request):
+        bread_count = Breed.objects.all().count()
+        cat_list = Cat.objects.all()
+        ctx = { "cat_list": cat_list, "bread_count": bread_count }
+        return render(request, self.template_name , ctx)
+
+class BreedList(LoginRequiredMixin, ListView):
+    model = Breed
+    template_name = "cats/bread_list.html"
 
 # CREATE
+class CatCreate(LoginRequiredMixin, CreateView):
+    model = Cat
+    fields = "__all__"
+    success_url = reverse_lazy("cats:cats")
+
+class BreedCreate(LoginRequiredMixin, CreateView):
+    model = Breed
+    fields = "__all__"
+    success_url = reverse_lazy("cats:cats")
 
 # UPDATE
+class CatUpdate(LoginRequiredMixin, UpdateView):
+    model = Cat
+    fields = "__all__"
+    success_url = reverse_lazy("cats:cats")
+
+class BreedUpdate(LoginRequiredMixin, UpdateView):
+    model = Breed
+    fields = "__all__"
+    success_url = reverse_lazy("cats:cats")
 
 # DELETE
+class CatDelete(LoginRequiredMixin, DeleteView):
+    model = Cat
+    fields = "__all__"
+    success_url = reverse_lazy("cats:cats")
+
+class BreedDelete(LoginRequiredMixin, DeleteView):
+    model = Breed
+    fields = "__all__"
+    success_url = reverse_lazy("cats:cats")
